9036019506596ab804e945389d4f9d002eab0c14
Ilya Gamyrkin <igamyrkin@vtb.ru>
2022-09-24 15:47:56 +0300

    feat: norm docs

--- src/components/NoPhoto/index.tsx
+++ src/components/NoPhoto/index.tsx
@@ -1,16 +1,8 @@
 import React from "react";
-import noPhoto from "../../assets/noPhoto.png";
 import "./style.css";
 
 const NoPhoto: React.FC<{ className?: string }> = ({ className, children }) => {
-  return (
-    <div
-      className={`${className} noPhotoImg`}
-      style={{ backgroundImage: `url(${noPhoto})` }}
-    >
-      {children}
-    </div>
-  );
+  return <div className={`${className} noPhotoImg`}>{children}</div>;
 };
 
 export default React.memo(NoPhoto);
--- src/pages/Create/index.tsx
+++ src/pages/Create/index.tsx
@@ -1,14 +1,8 @@
 import React from "react";
 import "./style.css";
-import unityJpg from "../../assets/unity.jpg";
 
 const Create = () => {
-  return (
-    <div
-      className="w-100 h-100 createImg"
-      style={{ backgroundImage: `url(${unityJpg})` }}
-    ></div>
-  );
+  return <div className="w-100 h-100 createImg"></div>;
 };
 
 export default React.memo(Create);
